b517236a78b15937a3aa4f34c2c89b6080a61205
Phil Yoo <philyoo@Phils-MacBook-Air.local>
2018-06-23 17:23:54 -0400

    built out a backend with jwt

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import TokenService from './TokenService';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
