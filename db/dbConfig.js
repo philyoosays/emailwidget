0201a39610e068f2e609185b2921be5d4463a201
Phil Yoo <philyoo@Phils-MacBook-Air.local>
2018-07-02 18:52:11 -0400

    still messing with db config

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
+
 const pgp = require('pg-promise')({
   query: q => console.log(q.query),
 });
 
-const dbConfig = pgp(process.env.DATABASE_URL || {
-  host:     process.env.DB_HOST || 'localhost',
-  port:     process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'emailwidget',
-})
+// const dbConfig = pgp(process.env.DATABASE_URL || {
+//   host:     process.env.DB_HOST || 'localhost',
+//   port:     process.env.DB_PORT || 5432,
+//   database: process.env.DB_NAME || 'emailwidget',
+// })
+
+const dbConfig = pgp(process.env.DATABASE_URL)
 
 module.exports = dbConfig;
